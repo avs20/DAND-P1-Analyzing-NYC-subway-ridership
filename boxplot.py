99e5ead020962a0fca7ba97ea8689c87be11250d
Ashutosh Singh <dh_ashu@hotmail.com>
2016-01-05 17:09:00 +0530

    Added the code to convert the variable as categorical and the plot generated

--- boxplot.py
+++ boxplot.py
@@ -1,6 +1,6 @@
 import pandas
 import numpy as np
-from ggplot import *
+from ggplot import ggplot, geom_histogram, ggtitle
 
 def floor_decade(date_value):
     #"Takes a date. Returns the decade."
@@ -12,10 +12,26 @@ data = data[data['ENTRIESn_hourly'] > 1]
 data_rain = data[data['rain'] > 0]
 data_no_rain = data[data['rain'] == 0]
 data_no_rain = data_no_rain[:9433]
-
+#
 #plot2 =  ggplot(data_no_rain , aes(x= 'ENTRIESn_hourly', color='rain', fill = 'rain')) + \
 #geom_histogram(binwidth = 50, alpha=0.6) + scale_x_continuous(limits=(0,10000)) + \
 #ggtitle('Subway Ridership on Non-Rainy Days ')+ xlab('Entries Each hour') + ylab('Frequency')
 
-ggplot(data , aes(x= 'hour', y='ENTRIESn')) + geom_boxplot()  + ggtitle('Subway Ridership on Non-Rainy Days ')+ xlab('Entries Each hour') + ylab('Frequency')
+#Eggplot(data , aes(x= 'hour', y='ENTRIESn')) + geom_boxplot()  + ggtitle('Subway Ridership on Non-Rainy Days ')+ xlab('Entries Each hour') + ylab('Frequency')
+
+#convert hour field to a categorical variable.
+
+data_no_rain['hour_cat'] = data_no_rain['hour'].astype('category')
+data_rain['hour_cat'] = data_rain['hour'].astype('category')
+data['hour_cat'] = data['hour'].astype('category')
+
+#now plot for data_rain and data_no_rain
+print ggplot(data_rain,aes(y = 'hour_cat',x='ENTRIESn_hourly'))+geom_boxplot()
+
+
+print ggplot(data_no_rain,aes(y = 'hour_cat',x='ENTRIESn_hourly'))+geom_boxplot()
+
+print ggplot(data,aes(y = 'hour_cat',x='ENTRIESn_hourly'))+geom_boxplot()
+
+
 
