9f0313eb813ec96cb0209c5fa8bf5ed2ef69760f
Ashutosh Singh <dh_ashu@outlook.com>
2016-01-11 07:59:33 +0530

    Adding somethings

--- boxplot.py
+++ boxplot.py
@@ -8,18 +8,17 @@ def floor_decade(date_value):
 
 data = pandas.read_csv('turnstile_weather_v2.csv')
 
-data = data[data['ENTRIESn_hourly'] > 1]
-data_rain = data[data['rain'] > 0]
-data_no_rain = data[data['rain'] == 0]
-data_no_rain = data_no_rain[:9433]
+#data_rain = data[data['rain'] > 0]
+#data_no_rain = data[data['rain'] == 0]
+#data_no_rain = data_no_rain[:9433]
 
 
 #convert hour field to a categorical variable.
 
-data_no_rain['hour_cat'] = data_no_rain['hour'].astype('category')
-data_rain['hour_cat'] = data_rain['hour'].astype('category')
-data['hour_cat'] = data['hour'].astype('category')
-data['rain'] = data['rain'].astype('category')
+#data_no_rain['hour_cat'] = data_no_rain['hour'].astype('category')
+#data_rain['hour_cat'] = data_rain['hour'].astype('category')
+#data['hour_cat'] = data['hour'].astype('category')
+#data['rain'] = data['rain'].astype('category')
 
 #now plot for data_rain and data_no_rain
 
@@ -27,9 +26,9 @@ data['rain'] = data['rain'].astype('category')
 
 
 #plot the data of both days as a facet
-plot =  ggplot(data,aes(y = 'hour_cat',x='ENTRIESn_hourly'))+ \
+plot =  ggplot(data,aes(y = 'hour',x='ENTRIESn_hourly'))+ \
     geom_boxplot() + \
-    facet_wrap('rain') + \
+    facet_grid('rain') + \
     xlab("Number of Entries")+ \
     ylab("Time of Day") + \
     ggtitle("Subway Ridership by Hour of Day on Rainy and Non-Rainy Days")+ \
